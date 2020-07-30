bf15a1a7fc3dea07a5eccfee8cd6931d51441316
QuarkL <745020259@qq.com>
2020-07-30 21:42:45 +0800

    submit

--- demos/form/app.py
+++ demos/form/app.py
@@ -3,11 +3,19 @@
 # @Date : 2020/07/29 21:03
 # @File : app.py
 # @Software : PyCharm
-from flask import Flask, render_template, flash, redirect, url_for, session, request
-from forms import LoginForm
+from flask import Flask, render_template, flash, redirect, url_for, session, request, send_from_directory
+from flask_ckeditor import random_filename, CKEditor
+
+from forms import LoginForm, UploadForm, RichTextForm
+from flask_wtf import FlaskForm
+import os
 
 app = Flask(__name__)
 app.secret_key = 'secret key'
+app.config['WTF_I18N_ENABLED'] = False
+app.config['UPLOAD_PATH'] = os.path.join(app.root_path, 'uploads')
+app.config['CKEDITOR_SERVE_LOCAL'] = True
+ckeditor = CKEditor(app)
 
 
 @app.route('/')
@@ -22,10 +30,44 @@ def index():
 
 @app.route('/basic', methods=['GET', 'POST'])
 def basic():
-    form = LoginForm()
-    if request.method == 'POST' and form.validate():
+    form = LoginForm(meta={'locales': ['zh']})
+    if form.validate_on_submit():
         username = form.username.data
         session['logged_in'] = True
         flash('Welcome home,%s!' % username)
         return redirect(url_for('index'))
     return render_template('basic.html', form=form)
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    form = UploadForm()
+    if form.validate_on_submit():
+        f = form.photo.data
+        filename = random_filename(f.filename)
+        f.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        flash('Upload success')
+        session['filenames'] = [filename]
+        return redirect(url_for('show_images'))
+    return render_template('upload.html', form=form)
+
+
+@app.route('/uploads/<path:filename>')
+def get_file(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
+
+
+@app.route('/uploaded-images')
+def show_images():
+    return render_template('uploaded.html')
+
+
+@app.route('/ckeditor', methods=['GET', 'POST'])
+def integrate_ckeditor():
+    form = RichTextForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        body = form.body.data
+        flash('Your post is published!')
+        return render_template('post.html', title=title, body=body)
+    return render_template('ckeditor.html', form=form)
--- demos/form/forms.py
+++ demos/form/forms.py
@@ -6,13 +6,29 @@
 from flask_ckeditor import CKEditorField
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms import Form,StringField, PasswordField, BooleanField, IntegerField, \
+from wtforms import Form, StringField, PasswordField, BooleanField, IntegerField, \
     TextAreaField, SubmitField, MultipleFileField
 from wtforms.validators import DataRequired, Length, ValidationError, Email
 
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired(), Length(8, 128)])
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')
+
+
+class UploadForm(FlaskForm):
+    photo = FileField('Upload Image', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg', 'gif'])])
+    submit = SubmitField()
+
+
+class MultiUploadForm(FlaskForm):
+    photo = MultipleFileField('Upload Image', validators={DataRequired()})
+    submit = SubmitField()
+
+
+class RichTextForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(1, 50)])
+    body = CKEditorField('Body', validators=[DataRequired()])
+    submit = SubmitField('Publish')
